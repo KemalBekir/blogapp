04f21d4467ec5ac0080624464f58faf015a383c0
Kemal <outsider.pld@gmail.com>
2023-09-07 23:30:10 +0100

    Updated - global.css, AuthLinks, CategoryList, Featured,Navbar, ThemeContext

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import styles from "./featured.module.css";
+import Image from "next/image";
 
 const Featured = () => {
-  return <div className={styles.container}>Featured</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hi, welcome article</b> Discover stories and creative ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="" fill />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis,
+            porro.
+          </h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+            distinctio reiciendis eum minima possimus magnam, vero atque dolore
+            harum facere nostrum. Placeat ullam animi maiores.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,13 +12,19 @@ const Navbar = () => {
         <Image src="/instagram.png" alt="instagram" width={24} height={24} />
         <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
         <Image src="/youtube.png" alt="youtube" width={24} height={24} />
-      </div> 
+      </div>
       <div className={styles.logo}>blog tutorial</div>
       <div className={styles.links}>
         <ThemeToggle />
-        <Link href="/">Homepage</Link>
-        <Link href="/">Contact</Link>
-        <Link href="/">About</Link>
+        <Link className={styles.link} href="/">
+          Homepage
+        </Link>
+        <Link className={styles.link} href="/">
+          Contact
+        </Link>
+        <Link className={styles.link} href="/">
+          About
+        </Link>
         <AuthLinks />
       </div>
     </div>
